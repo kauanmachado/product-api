e06038c8bd3dfcce48c5773b8c353c17ea16d06c
Kauan Machado <kauanmachad0@outlook.com>
2025-01-22 17:23:50 -0300

    first commit

---
+++ src/main/java/com/kauanmachado/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.kauanmachado.produtosapi.repository;
+
+import com.kauanmachado.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
